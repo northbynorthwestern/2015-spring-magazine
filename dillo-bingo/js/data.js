494fa2686a6d60a03734cc3595bb0348836ea45f
AditiBhandari <apple@dhcp-10-105-188-28.wireless.northwestern.private>
2015-05-26 01:25:55 -0500

    push all the NBNN stories

---
+++ dillo-bingo/js/data.js
@@ -0,0 +1,33 @@
+
+var headerText = "Dillo Bingo 2.0";
+var introText = "<h2>If you win this game, you did Dillo Day right.</h2><h3><strong>by NBN Staff</strong></h3><h4>Design and Development: Aditi Bhandari</h4><h4><strong>Published on May 25, 2015</strong></h4><center><hr></center><p>Dillo Day is the one day of the year when Northwestern students can be as absurd and debaucherous as they please without receiving any judgment. For most, that means getting obliterated at 9 a.m. and passing out before the concert even starts. But we at NBN know that many of you ambitious future world leaders have greater aspirations &#8212; which is why we've returned for the second edition of Dillo Bingo! This game is not for the faint of heart, but for those who truly want to get the most out of their Dillo Day experience. Remember, Dillo is a marathon, not a sprint. But if you sprint through it, you'll finish before everyone else.</p>";
+
+var winText = "<p>You win!</p>";
+
+var JSONBingo = {"squares": [
+        {"square": "Shotgun a Four Loko"},
+        {"square": "Create a condom balloon"},
+        {"square": "Find sorority women taking photos in matching shirts"},
+        {"square": "Find Morty"},
+        {"square": "Take a selfie with a couple making out in the background"},
+        {"square": "Call your parents"},
+        {"square": "Streak"},
+        {"square": "Do a body shot"},
+        {"square": "Climb onto the roof of an academic building"},
+        {"square": "Flash a TA"},
+        {"square": "Take a selfie with Justin Barbin"},
+        {"square": "End up at the Bah&#224;&#39;&#237; Temple"},
+        {"square": "Get or give an over-the-pants hand job"},
+        {"square": "Drunkenly fill out CTECs"},
+        {"square": "Jump in the lake"},
+        {"square": "Participate in a flash mob"},
+        {"square": "Brush your teeth with a bottle of Jack"},
+        {"square": "Find people playing frisbee. Steal their frisbee."},
+        {"square": "Perform the 30-hour DM dance"},
+        {"square": "Do the Macarena at a wildly inappropriate time"},
+        {"square": "Run across the stage"},
+        {"square": "Take five photos of Mayfest people looking too busy to care about you"},
+        {"square": "Shotgun a beer with a professor"},    	
+        {"square": "Peel through two layers of sunburnt skin"}, 	
+    ]
+};
